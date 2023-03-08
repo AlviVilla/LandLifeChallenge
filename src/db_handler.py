afd6e5f43af759c64e6fd1fe5ebf0c171ff34599
AlviVilla <a.villa6@hotmail.com>
2023-03-08 09:07:40 +0000

    Minor corrections in PEP8 standards

--- src/db_handler.py
+++ src/db_handler.py
@@ -7,11 +7,18 @@ import csv
 class DB_Handler:
 
     def __init__(self, conf):
+        """Constructor to injest the data into de database
+        Actions:
+            DB creation
+            Tables creation
+            Retrieval of CSV data
+            insertion of CSV data into tables
+        """
         self.create_DB(conf)
         self.create_tables(conf)
-        f_data = self.data_retrieval(conf["data"]["field_data"])
+        tree_data = self.data_retrieval(conf["data"]["field_data"])
         specs_data = self.data_retrieval(conf["data"]["species"])
-        self.insert_in_field_data(conf, f_data)
+        self.insert_in_field_data(conf, tree_data)
         self.insert_in_species(conf, specs_data)
 
     def create_DB(self, conf):
@@ -19,10 +26,10 @@ class DB_Handler:
         Param: 
             conf: configuration file
         """
-        con = pymysql.connect(host=conf["database"]["hostname"], user=conf["database"]["user"], password=conf["database"]["password"])
+        con = pymysql.connect(host = conf["database"]["hostname"], user = conf["database"]["user"], password = conf["database"]["password"])
         cur = con.cursor()
         cur.execute("SHOW DATABASES")
-        dbs=cur.fetchall()
+        dbs = cur.fetchall()
         if conf["database"]["db_name"] not in str(dbs):
             cur.execute("CREATE DATABASE "+ conf["database"]["db_name"])
         cur.close()
@@ -32,10 +39,10 @@ class DB_Handler:
         Param: 
             conf: configuration file
         """
-        con = pymysql.connect(host=conf["database"]["hostname"], user=conf["database"]["user"], password=conf["database"]["password"], db=conf["database"]["db_name"])
+        con = pymysql.connect(host = conf["database"]["hostname"], user = conf["database"]["user"], password = conf["database"]["password"], db = conf["database"]["db_name"])
         cur = con.cursor()
         cur.execute("SHOW TABLES")
-        tables=cur.fetchall()
+        tables = cur.fetchall()
         if 'field_data' not in str(tables):
             cur.execute("CREATE TABLE field_data (individual_tree_id int, species_id int, method varchar(32), height int, health int, year_monitored int, PRIMARY KEY (individual_tree_id, year_monitored), UNIQUE (individual_tree_id, year_monitored))")
         if 'species' not in str(tables):
@@ -51,9 +58,9 @@ class DB_Handler:
         """
         data = requests.get(url).content.decode('utf-8')
         if ';' in str(data):
-            csv_reader = csv.DictReader(data.splitlines(), delimiter=';')
+            csv_reader = csv.DictReader(data.splitlines(), delimiter = ';')
         elif ',' in str(data):
-            csv_reader = csv.DictReader(data.splitlines(), delimiter=',')
+            csv_reader = csv.DictReader(data.splitlines(), delimiter = ',')
         return csv_reader
 
     def insert_in_field_data(self, conf, data):
@@ -62,9 +69,9 @@ class DB_Handler:
             conf: Configuration data
             data: DictReader object with CSV data
         """
-        con = pymysql.connect(host=conf["database"]["hostname"], user=conf["database"]["user"], password=conf["database"]["password"], db=conf["database"]["db_name"])
+        con = pymysql.connect(host = conf["database"]["hostname"], user = conf["database"]["user"], password = conf["database"]["password"], db = conf["database"]["db_name"])
         for tab in data:
-            cur=con.cursor()
+            cur = con.cursor()
             sql = """INSERT IGNORE INTO field_data (individual_tree_id, species_id, method, height, health, year_monitored)
             VALUES (%s, %s, %s, %s, %s, %s) 
             """
@@ -78,11 +85,11 @@ class DB_Handler:
             conf: Configuration data
             data: DictReader object with CSV data
         """
-        con = pymysql.connect(host=conf["database"]["hostname"], user=conf["database"]["user"], password=conf["database"]["password"], db=conf["database"]["db_name"])
+        con = pymysql.connect(host = conf["database"]["hostname"], user = conf["database"]["user"], password = conf["database"]["password"], db = conf["database"]["db_name"])
         for tab in data:
-            cur=con.cursor()
+            cur = con.cursor()
 
-            sql="""INSERT IGNORE INTO species (tree_species_id, latin_name)
+            sql = """INSERT IGNORE INTO species (tree_species_id, latin_name)
             VALUES (%s, %s)
             """
             cur.execute(sql,(tab["tree_species_id"],tab["latin_name"]))
@@ -97,11 +104,11 @@ class DB_Handler:
         highest = []
         for a in result_tuple:
             highest.append({"individual_tree_id": a[0], "height": a[3]})
-        output= {
+        output = {
             "year": result_tuple[0][-1],
             "highest_trees": highest
         }
-        return (output)
+        return output
 
     def read_best_method(self, result_tuple1, result_tuple2):
         """Formatting of the resultant SQL query to JSON for the best method
@@ -110,18 +117,18 @@ class DB_Handler:
                 expected fields (method, average)
             result_tuple2: second match tuple returned by cursor
         """
-        seen_in_list = []
+        seen_in_list  = []
         for a in result_tuple2:
             seen_in_list.append({"individual_tree_id": a[0], "year": a[-3], "health": a[4]})
 
-        output= {
+        output = {
             "tree_species_id": result_tuple2[0][1],
-            "tree_species_latin_name":result_tuple2[0][-1],
-            "best_method":result_tuple1[0][0],
-            "health_average":str(result_tuple1[0][1]),
-            "seen_in":seen_in_list
+            "tree_species_latin_name": result_tuple2[0][-1],
+            "best_method": result_tuple1[0][0],
+            "health_average": str(result_tuple1[0][1]),
+            "seen_in": seen_in_list
         }
-        return (output)
+        return output
 
 
     def cursor_select(self, conf, query):
@@ -130,7 +137,7 @@ class DB_Handler:
             conf: Configuration data
             query: String value with SQL query
         """
-        con = pymysql.connect(host=conf["database"]["hostname"], user=conf["database"]["user"], password=conf["database"]["password"], db=conf["database"]["db_name"])
+        con = pymysql.connect(host = conf["database"]["hostname"], user = conf["database"]["user"], password = conf["database"]["password"], db = conf["database"]["db_name"])
         with con.cursor() as cur:
             cur.execute(query)
             queryRes = cur.fetchall()
--- src/main.py
+++ src/main.py
@@ -11,13 +11,13 @@ with open(dirname + '/../conf/conf.json') as file:
     conf = json.load(file)
 
 #   Create new DB instance and initialize DB class
-db= DB_Handler(conf)
+db = DB_Handler(conf)
 
 
 app = Flask(__name__)
 
-@app.route("/highest-trees", methods=["GET",  "POST"], defaults={'year': 2021})
-@app.route("/highest-trees/<year>", methods=["GET",  "POST"])
+@app.route("/highest-trees", methods = ["GET",  "POST"], defaults = {'year': 2021})
+@app.route("/highest-trees/<year>", methods = ["GET",  "POST"])
 def highest_trees(year):
     """Method to define the highest-trees endpoint
     Accepted Operations: GET, POST
@@ -27,13 +27,13 @@ def highest_trees(year):
         The endpoint will be exposed with a default value of year: 2021
     Returns list of highest trees and year in JSON format
     """
-    query= 'SELECT * FROM field_data WHERE year_monitored = '+str(year)+' ORDER BY height DESC LIMIT 5'
-    result=db.cursor_select(conf,query)
+    query = 'SELECT * FROM field_data WHERE year_monitored = '+ str(year) +' ORDER BY height DESC LIMIT 5'
+    result = db.cursor_select(conf,query)
     print(result[0][-1])
     return db.read_highest(result)
 
-@app.route("/best-method-for-species", methods=["GET", "POST"],  defaults={'species_id': 281})
-@app.route("/best-method-for-species/<species_id>", methods=["GET", "POST"])
+@app.route("/best-method-for-species", methods = ["GET", "POST"],  defaults = {'species_id': 281})
+@app.route("/best-method-for-species/<species_id>", methods = ["GET", "POST"])
 def best_method(species_id):
     """Method to define the best method endpoint
     Accepted Operations: GET, POST
@@ -43,15 +43,15 @@ def best_method(species_id):
         If no species_id is specified, defaults to 281
     Returns list in JSON format
     """
-    query= 'SELECT method, AVG (health) from field_data  WHERE species_id = '+str(species_id)+' GROUP BY method, species_id ORDER BY AVG(health) DESC LIMIT 1'
+    query = 'SELECT method, AVG (health) from field_data  WHERE species_id = '+ str(species_id) +' GROUP BY method, species_id ORDER BY AVG(health) DESC LIMIT 1'
     first_match = db.cursor_select(conf, query)
-    query2= 'SELECT * FROM field_data LEFT JOIN species ON field_data.species_id=species.tree_species_id WHERE species_id = '+str(species_id)+' AND method = "'+str(first_match[0][0])+'"'
+    query2= 'SELECT * FROM field_data LEFT JOIN species ON field_data.species_id=species.tree_species_id WHERE species_id = '+ str(species_id) +' AND method = "'+ str(first_match[0][0]) +'"'
     second_match = db.cursor_select(conf, query2)
     return db.read_best_method(first_match, second_match)
 
 #   Run the Flask application
 app.run(
-    debug=False,
-    port=conf["service_port"],
-    host=conf["service_host"]
+    debug = False,
+    port = conf["service_port"],
+    host = conf["service_host"]
 )
